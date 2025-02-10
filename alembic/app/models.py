2cdfb603c3ca5d409f3a253e9c149848ca311d6c
Matthew Prestifilippo <matt.prestifilippo@verantos.com>
2025-02-10 17:15:22 -0500

    alembic - table arg functionality for schema change

--- alembic/app/models.py
+++ alembic/app/models.py
@@ -7,8 +7,10 @@ Base = declarative_base()
 
 class Test(Base):
     __tablename__ = 'test'
+    __table_args__ = {'schema': 'public'}
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String, nullable=False)
+    age = Column(Integer)
 
 engine = create_engine(os.getenv("DATABASE_URL"))
 SessionLocal = sessionmaker(bind=engine)
